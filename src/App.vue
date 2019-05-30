4de7fa9995b7a2351590e04668e00a2ced821562
Paul RASTOIN <prastoin@student.42.fr>
2019-05-30 08:12:51 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <span class="title">Domos.</span>
+      <div>
+        <span class="allTxt headerContent">Nos appartements.</span>
+        <span class="allTxt headerContent">Mes coups de Coeur.</span>
+        <span class="allTxt headerContent">Me contacter.</span>
+      </div>
+      <div class="containerDeco">
+        <div class="headerDeco" style="width: 20px; height: 20px;"></div>
+        <div class="headerDeco" style="width: 40px; height: 40px;"></div>
+      </div>
+    </header>
+    <home></home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import home from "./components/home.vue";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    home
   }
 }
 </script>
@@ -23,6 +34,60 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.allTxt {
+  font-size: 1rem;
+}
+
+body {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  line-height: 1.5;
+}
+</style>
+
+<style scoped>
+
+header {
+  position: relative;
+  padding: 16px;
+  background: #24292e;
+  display: flex;
+  align-items: baseline;
+  align-content: flex-end;
+}
+
+.title {
+  color: white;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-right: 32px;
+}
+
+.headerContent {
+  color: silver;
+}
+
+.headerContent:not(:first-child){
+  margin-left: 12px;
+}
+
+.headerDeco {
+  border-radius: 50%;
+  background: white;
+  align-self: center;
+  justify-content: flex-end;
+}
+
+.containerDeco {
+  position: absolute;
+  display: block;
+  display: flex;
+  right: 16px;
+  align-self: center;
+}
+
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="root">
+        <div class="container">
+            <p class="welcome">Bienvenue</p> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home"
+}
+</script>
+
+<style scoped>
+.container {
+    background: silver;
+}
+
+.welcome {
+    max-width: 1280px;
+    margin-right: auto;
+    margin-left: auto;
+    background: white;
+    font-size: 1.5rem;
+}
+</style>
